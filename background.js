dd1c5f9bd1ee77dbf8b27e9ecc47de4e33388683
Santanu Mukherjee <gopinathenterprisetechnology@gmail.com>
2026-07-17 18:26:07 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 // ব্যাকগ্রাউন্ড সার্ভিস যা ট্যাব মেসেজিং কন্ট্রোল করে
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Banglarbhumi Auto Sync Extension Ready.");
+chrome.action.onClicked.addListener((tab) => {
+  chrome.tabs.create({ url: chrome.runtime.getURL("popup.html") });
 });
+
